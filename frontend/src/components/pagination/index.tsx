de5c497fd60c72addaf6037adf5be405caeba301
Rubenslourenco <alvesrubens926@gmail.com>
2021-09-13 00:38:15 -0300

    update pagination

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -11,14 +11,14 @@ const Pagination = ( {page, OnPageChance} : Props) => {
         <div className="row d-flex justify-content-center">
         <nav>
             <ul className="pagination">
-                <li className={`page-item ${page.first ? 'disable' : ''}`}>
+                <li className={`page-item ${page.first ? 'disabled' : ''} `}>
                     <button className="page-link"onClick={() => OnPageChance(page.number - 1)}>Anterior</button>
                 </li>
                 <li className="page-item disabled">
                     <span className="page-link">{page.number + 1}</span>
                 </li>
-                <li className={`page-item ${page.last ? 'disable' : ''}`}>
-                    <button className="page-link"onClick={() => OnPageChance(page.number - 1)}>Próxima</button>
+                <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                    <button className="page-link"onClick={() => OnPageChance(page.number + 1)}>Próxima</button>
                 </li>
             </ul>
         </nav>
